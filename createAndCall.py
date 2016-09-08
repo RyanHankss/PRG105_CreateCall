fe765dbe8f099ff5d7d506396a9e487d38754f92
RyanHankss <rhanks@mchenry.edu>
2016-09-08 12:48:36 -0500

    Add files via upload
    Create a function and Call it 3 times

---
+++ createAndCall.py
@@ -0,0 +1,12 @@
+def raven_poe():
+    print("Then the bird said Nevermore.")
+    print("Insanity inducing I do implore")
+    print("Quoth the Raven Nevermore.")
+    print("There time and life and so much more")
+    print("Quoth the Raven Nevermore.")
+
+raven_poe()
+
+raven_poe()
+
+raven_poe()
